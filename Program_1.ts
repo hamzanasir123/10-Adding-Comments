3e29f27ad0ac0d77ef24686c5f246a27b25a6766
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-03-13 14:22:06 +0500

    first commit

---
+++ Program_1.ts
@@ -0,0 +1,9 @@
+
+
+// Program written by : Hamza Nasir ID/00245369
+// Current Date : 24-2-2024
+
+// This program will write Hello World
+
+console.log('Hello World');
+
---
+++ Program_2.js
@@ -0,0 +1,5 @@
+"use strict";
+// Program written by : Hamza Nasir ID/00245369
+// Current Date : 24-2-2024
+// This program will add two digits.
+console.log(2 + 2);
